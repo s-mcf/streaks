57524c1ba0198df05a1f444132a08c5d27013539
Simon McFarlane <sm@desu.ne.jp>
2023-12-12 18:16:56 -0800

    Add streakgrid style

--- client/src/StreakGrid.tsx
+++ client/src/StreakGrid.tsx
@@ -15,26 +15,25 @@ const StreakGrid: React.FC<StreakGridProps> = ({ currentStreak }) => {
         for (let day = 0; day < 7; day++) {
             const isFilled = week * 7 + day < currentStreak;
             days.push(
-                <div key={week * 7 + day} className={`circle ${isFilled ? 'filled' : ''}`}></div>
+                <div key={week * 7 + day} className={`h-6 w-6 md:h-8 md:w-8 rounded-full ${isFilled ? 'bg-green-500' : 'bg-gray-200'} m-1`}></div>
             );
         }
-        grid.push(<div key={week} className="week">{days}</div>);
+        grid.push(<div key={week} className="flex justify-center">{days}</div>);
     }
 
     // Add an extra row if the current streak is a multiple of 7
     if (currentStreak % 7 === 0) {
         const emptyWeek = Array.from({ length: 7 }, (_, index) => (
-            <div key={weeks * 7 + index} className="circle"></div>
+            <div key={weeks * 7 + index} className="h-6 w-6 md:h-8 md:w-8 rounded-full bg-gray-200 m-1"></div>
         ));
-        grid.push(<div key={weeks} className="week">{emptyWeek}</div>);
+        grid.push(<div key={weeks} className="flex justify-center">{emptyWeek}</div>);
     }
 
     return (
-        <div className="streak-grid">
+        <div className="flex flex-col items-center my-4">
             {grid}
         </div>
     );
 };
 
 export default StreakGrid;
-
